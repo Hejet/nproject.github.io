fee906ef0339c01e70c89b9b24e2a4bbf6cf5bb8
Vladimir <chuwoza313@gmail.com>
2020-10-14 22:35:28 +0300

    finish calc

--- js/script_2.js
+++ js/script_2.js
@@ -1,19 +1,91 @@
-function logValue(id) {
-  var el = document.getElementById(id);
-  console.log(el.value);
+// function logValue(id) {
+//   var el = document.getElementById(id);
+//   console.log(el.value);
+// }
+
+// function getValue(id) {
+//   var el = document.getElementById(id);
+//   return el.value;
+// }
+
+// var firsrNameId = 'first-name';
+// var lastNameId = 'last-name';
+
+// logValue(firsrNameId);
+// logValue(lastNameId);
+
+// var value = getValue(lastNameId);
+
+// window.alert(value);
+
+// addErrorClass ('last-name');
+
+// Выполнить функцию прямо сейчас
+// addErrorClassAllInputs();
+
+// Отсрочить выполнение функции
+// window.setTimeout(addErrorClassToAllInputs, 2000);
+
+// function addErrorClass(elementID) {
+//   var fistName = document.getElementById('first-name');
+//   var lastName = document.getElementById('lastst-name');
+//   var address = document.getElementById('address');
+// }
+
+// function addErrorClass(elementId) {
+//   var element = document.getElementById(elementId);
+//   element.className = 'error-input';
+// }
+
+// function addErrorClassToAllInputs() {
+//   addErrorClass('first-name');
+//   addErrorClass('last-name');
+//   addErrorClass('address');
+// }
+
+// ! События
+
+var sendButton = document.getElementById('button');
+
+sendButton.addEventListener('click', addErrorClassToAllInputs);
+
+// IMAGE
+var imageButtonVanja = document.getElementById('button-img-vanja');
+var imageButtonMama = document.getElementById('button-img-mama');
+var imageButtonPapa = document.getElementById('button-img-papa');
+var imageButtonGrmAnja = document.getElementById('button-img-grmAnja');
+var imageButtonCat = document.getElementById('button-img-cat');
+
+
+function changeImageButtonVanja() {
+	var imageEl = document.getElementById('avatar');
+		// imageEl.src ='https://scandinews.fi/assets/image-cache/images/2015/4/0/1-0.246.2ee62299.jpg';
+		imageEl.src ='./img/AveIvan.jpg';
+		imageEl.title = 'Ваня!';
 }
-
-function getValue(id) {
-  var el = document.getElementById(id);
-  return el.value;
+function changeImageButtonMama() {
+	var imageEl = document.getElementById('avatar');
+		imageEl.src ='./img/mama.jpg';
+		imageEl.title = 'МАМА!';
 }
+function changeImageButtonPapa() {
+	var imageEl = document.getElementById('avatar');
+		imageEl.src ='./img/papa.jpg';
+		imageEl.title = 'ПАПА!';
+}
+function changeImageButtonGrmAnja() {
+	var imageEl = document.getElementById('avatar');
+		imageEl.src ='./img/grandmotherAnja.jpg';
+		imageEl.title = 'Бабушка Аня!';
+}
+function changeImageButtonCat() {
+	var imageEl = document.getElementById('avatar');
+		imageEl.src ='https://scandinews.fi/assets/image-cache/images/2015/4/0/1-0.246.2ee62299.jpg';
+		imageEl.title = 'КОТ!';
+}
+imageButtonVanja.addEventListener ('click', changeImageButtonVanja);
+imageButtonMama.addEventListener ('click', changeImageButtonMama);
+imageButtonPapa.addEventListener ('click', changeImageButtonPapa);
+imageButtonGrmAnja.addEventListener ('click', changeImageButtonGrmAnja);
+imageButtonCat.addEventListener ('click', changeImageButtonCat);
 
-var firsrNameId = 'first-name';
-var lastNameId = 'last-name';
-
-logValue(firsrNameId);
-logValue(lastNameId);
-
-var value = getValue(lastNameId);
-
-window.alert(value);
--- js/script_3.js
+++ js/script_3.js
@@ -1,85 +1,49 @@
-function addErrorClass(elementID) {
-  var fistName = document.getElementById('first-name');
-  var lastName = document.getElementById('lastst-name');
-  var address = document.getElementById('address');
+// ! Калькулятор
+
+// ? Найти элементы по ID, после создать массив
+
+// var buttonPlus = document.getElementById('buttonPlus');
+// var buttonMinus = document.getElementById('buttonMinus');
+// var buttonMultyply = document.getElementById('buttonMultyply');
+// var buttonDevide = document.getElementById('buttonDevide');
+
+// var operationButons = [buttonPlus, buttonMinus, buttonMultyply, buttonDevide];
+
+//  ? Найти элементы по Классу (Нужно что бы у них был одинаковый класс)
+
+var operationButons = document.getElementsByClassName('operation-button');
+
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+	
+
+function makeOperation(operationCode) {
+	
+	var number1 = Number(input1.value)
+	var number2 = Number(input2.value)
+	
+	if (operationCode === '+'){
+		var result = Number(input1.value) + Number(input2.value);
+	}
+	else if (operationCode === '-'){
+		var result = Number(input1.value) - Number(input2.value);
+	} else if(operationCode === '*'){
+		var result = Number(input1.value) * Number(input2.value);
+	} else if (operationCode === '/'){
+		var result = Number(input1.value) / Number(input2.value);
+	} else {
+		window.alert('operation is uncnown')
+	}
+	window.alert("Ваш ответ: " + result)
 }
 
-function addErrorClass(elementId) {
-  var element = document.getElementById(elementId);
-  element.className = 'error-input';
+function onOperationButtonClick(elementObject) {
+	var clickedElement = elementObject.currentTarget;
+	var operation = clickedElement.innerHTML;
+	makeOperation(operation)
 }
 
-function addErrorClassToAllInputs() {
-  addErrorClass('first-name');
-  addErrorClass('last-name');
-  addErrorClass('address');
+for (var i = 0; i < operationButons.length; i++) {
+	var button = operationButons [i];
+	button.addEventListener('click', onOperationButtonClick)
 }
-
-// addErrorClass ('last-name');
-
-// Выполнить функцию прямо сейчас
-// addErrorClassAllInputs();
-
-// Отсрочить выполнение функции
-// window.setTimeout(addErrorClassToAllInputs, 2000);
-
-// события
-
-var sendButton = document.getElementById('button');
-
-sendButton.addEventListener('click', addErrorClassToAllInputs);
-
-// IMAGE
-
-// avatarEl.src =
-//   'https://scandinews.fi/assets/image-cache/images/2015/4/0/1-0.246.2ee62299.jpg';
-
-// avatarEl.title = 'Теперь я Кот!';
-
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMultyply = document.getElementById('buttonMultyply');
-var buttonDevide = document.getElementById('buttonDevide');
-
-function onButtonClickPlus() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-  var number1 = input1.value;
-  var number2 = input2.value;
-  var summ = Number(number1) + Number(number2);
-  console.log(summ);
-}
-function onButtonClickMinus() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-  var number1 = input1.value;
-  var number2 = input2.value;
-  var summ = Number(number1) - Number(number2);
-  console.log(summ);
-}
-function onButtonClickMyltyply() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-  var number1 = input1.value;
-  var number2 = input2.value;
-  var summ = Number(number1) * Number(number2);
-  console.log(summ);
-}
-function onButtonClickDevide() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-  var number1 = input1.value;
-  var number2 = input2.value;
-  var summ = Number(number1) / Number(number2);
-  console.log(summ);
-}
-
-// onButtonClick(buttonPlus);
-// onButtonClick(buttonMinus);
-// onButtonClick(buttonMultyply);
-// onButtonClick(buttonDevide);
-
-buttonPlus.addEventListener('click', onButtonClickPlus);
-buttonMinus.addEventListener('click', onButtonClickMinus);
-buttonMultyply.addEventListener('click', onButtonClickMyltyply);
-buttonDevide.addEventListener('click', onButtonClickDevide);
