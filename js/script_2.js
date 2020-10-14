002f33957e12a4989b002af37e87694de29a3e2c
Vladimir <chuwoza313@gmail.com>
2020-10-14 22:48:18 +0300

    fixing js error-class

--- js/script_2.js
+++ js/script_2.js
@@ -26,22 +26,22 @@
 // Отсрочить выполнение функции
 // window.setTimeout(addErrorClassToAllInputs, 2000);
 
-// function addErrorClass(elementID) {
-//   var fistName = document.getElementById('first-name');
-//   var lastName = document.getElementById('lastst-name');
-//   var address = document.getElementById('address');
-// }
+function addErrorClass(elementID) {
+  var fistName = document.getElementById('first-name');
+  var lastName = document.getElementById('lastst-name');
+  var address = document.getElementById('address');
+}
 
-// function addErrorClass(elementId) {
-//   var element = document.getElementById(elementId);
-//   element.className = 'error-input';
-// }
+function addErrorClass(elementId) {
+  var element = document.getElementById(elementId);
+  element.className = 'error-input';
+}
 
-// function addErrorClassToAllInputs() {
-//   addErrorClass('first-name');
-//   addErrorClass('last-name');
-//   addErrorClass('address');
-// }
+function addErrorClassToAllInputs() {
+  addErrorClass('first-name');
+  addErrorClass('last-name');
+  addErrorClass('address');
+}
 
 // ! События
 
