d267b5c7db122978b2e9a3de69d62314d2d4d457
Vladimir <chuwoza313@gmail.com>
2020-10-19 23:35:19 +0300

    add slider object

--- js/IT_KAM/slider.js
+++ js/IT_KAM/slider.js
@@ -1,33 +1,52 @@
-// ! find elements
-let showPrewBtn = document.getElementById('show-prev-btn'),
-		showNextBtn = document.getElementById('show-next-btn'),
-		slideImage = document.getElementById('slide-img');
+// var imagesUrls = [];
+// let currentImageIndex = 0;
+// // ! find elements
+// let showPrevBtn = document.getElementById('show-prev-btn'),
+// 	showNextBtn = document.getElementById('show-next-btn'),
+// 	slideImage = document.getElementById('slide-img');
 
-//! Subscribe to events
-
-showPrewBtn.addEventListener('click', onShowPrevClick);
-showNextBtn.addEventListener('click', onShowNextClick);
-
-
-// ! create image array
-var imagesUrls = [];
-imagesUrls.push('https://img.drive.ru/i/0/5b7fd52eec05c4804a000068.jpg');
-imagesUrls.push('https://cdnimg.rg.ru/i/gallery/822747f4/6_7f5b75b1.jpg');
-imagesUrls.push('https://wroom.ru/i/news/10932img1.jpg');
-imagesUrls.push('https://s0.rbk.ru/v6_top_pics/media/img/4/39/755300180767394.jpg');
-
-let currentImageIndex = 0;
-slideImage.src = imagesUrls[currentImageIndex];
+// //! Subscribe to events
+// showPrevBtn.addEventListener('click', onShowPrevClick);
+// showNextBtn.addEventListener('click', onShowNextClick);
 
 
+// // ! create image array
+// imagesUrls.push('https://img.drive.ru/i/0/5b7fd52eec05c4804a000068.jpg');
+// imagesUrls.push('https://cdnimg.rg.ru/i/gallery/822747f4/6_7f5b75b1.jpg');
+// imagesUrls.push('https://wroom.ru/i/news/10932img1.jpg');
+// imagesUrls.push('https://s0.rbk.ru/v6_top_pics/media/img/4/39/755300180767394.jpg');
 
-// ! Function definitions
+// slideImage.src = imagesUrls[currentImageIndex];
+// showPrevBtn.disabled = true;
+// showPrevBtn.innerHTML = "STOP";
 
-function onShowPrevClick() {
-	currentImageIndex--;
-	slideImage.src = imagesUrls[currentImageIndex];
-}
-function onShowNextClick() {
-	currentImageIndex++;
-	slideImage.src = imagesUrls[currentImageIndex];
-}
+
+// // ! Function definitions
+
+// function onShowPrevClick() {
+// 	currentImageIndex--;
+// 	slideImage.src = imagesUrls[currentImageIndex];
+// 	showNextBtn.disabled = false;
+// 	showNextBtn.innerHTML = "NEXT";
+
+// 	// disabled prev Button if need
+// 	if (currentImageIndex === 0) {
+// 		showPrevBtn.disabled = true;
+// 		showPrevBtn.innerHTML = "STOP";
+// 	}
+// }
+// function onShowNextClick() {
+// 	currentImageIndex++;
+// 	slideImage.src = imagesUrls[currentImageIndex];
+// 	showPrevBtn.disabled = false;
+// 	showPrevBtn.innerHTML = "PREV";
+
+// 	// disabled next Button if need
+// 	if (currentImageIndex === (imagesUrls.length - 1)) {
+// 		showNextBtn.disabled = true;
+// 		showNextBtn.innerHTML = "STOP";
+// 	}
+
+// }
+
+slider.start();
---
+++ js/IT_KAM/slider_obj.js
@@ -0,0 +1,56 @@
+var slider = {
+	imagesUrls: [],
+	currentImageIndex: 0,
+	// ! find elements
+	showPrevBtn: document.getElementById('show-prev-btn'),
+	showNextBtn: document.getElementById('show-next-btn'),
+	slideImage: document.getElementById('slide-img'),
+
+	start: function () {
+		var that = this;
+		this.showPrevBtn.addEventListener('click', function (e) {
+			that.onShowPrevClick(e);
+		});
+		this.showNextBtn.addEventListener('click', function (e) {
+			that.onShowNextClick(e);
+		});
+
+
+		// ! create image array
+		this.imagesUrls.push('https://img.drive.ru/i/0/5b7fd52eec05c4804a000068.jpg');
+		this.imagesUrls.push('https://cdnimg.rg.ru/i/gallery/822747f4/6_7f5b75b1.jpg');
+		this.imagesUrls.push('https://wroom.ru/i/news/10932img1.jpg');
+		this.imagesUrls.push('https://s0.rbk.ru/v6_top_pics/media/img/4/39/755300180767394.jpg');
+
+		this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+		this.showPrevBtn.disabled = true;
+		this.showPrevBtn.innerHTML = "STOP";
+	},
+
+	// ! Function definitions
+
+	onShowPrevClick: function (e) {
+		this.currentImageIndex--;
+		this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+		this.showNextBtn.disabled = false;
+		this.showNextBtn.innerHTML = "NEXT";
+
+		// disabled prev Button if need
+		if (this.currentImageIndex === 0) {
+			this.showPrevBtn.disabled = true;
+			this.showPrevBtn.innerHTML = "STOP";
+		}
+	},
+	onShowNextClick: function (e) {
+		this.currentImageIndex++;
+		this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+		this.showPrevBtn.disabled = false;
+		this.showPrevBtn.innerHTML = "PREV";
+
+		// disabled next Button if need
+		if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
+			this.showNextBtn.disabled = true;
+			this.showNextBtn.innerHTML = "STOP";
+		}
+	}
+}
